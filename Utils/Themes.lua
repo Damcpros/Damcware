a017b6bf3b7dbbc63e467e2faa541f2ffe9b43a6
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2024-07-25 21:54:02 -0400

    Create Themes.lua

---
+++ Utils/Themes.lua
@@ -0,0 +1,16 @@
+local Themes = {
+    ["Green"] = {
+        Color1 = Color3.fromRGB(229, 255, 0),
+        Color2 = Color3.fromRGB(34, 255, 0),
+    },
+    ["Blue"] = {
+        Color1 = Color3.fromRGB(48, 131, 255),
+        Color2 = Color3.fromRGB(35, 98, 186),
+    },
+    ["CottonCandy"] = {
+        Color1 = Color3.fromRGB(106, 190, 255),
+        Color2 = Color3.fromRGB(204, 152, 255),
+    },
+}
+
+return Themes
